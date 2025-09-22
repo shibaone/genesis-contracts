04e2dff208ffd045eb490dcafa6b323381528a80
Vinayak <vinayak@shib.io>
2025-09-23 01:50:33 +0530

    fix

--- generate-genesis.js
+++ generate-genesis.js
@@ -29,7 +29,7 @@ program.parse(process.argv)
 async function compileContract(key, contractFile, contractName, solcVersion) {
   return new Promise((resolve, reject) => {
     const ls = spawn(
-      `solc-select use ${solcVersion} && solc --bin-runtime openzeppelin-solidity/=node_modules/openzeppelin-solidity/ /=/ ${contractFile}`,
+      `~/.local/bin/solc-select use ${solcVersion} && solc --bin-runtime openzeppelin-solidity/=node_modules/openzeppelin-solidity/ /=/ ${contractFile}`,
       { shell: true }
     )
 
